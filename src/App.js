676d6e5fa86ce9f001f6203aa3fe97b02c80f63d
Jason Acido <jasonacido@gmail.com>
2022-09-09 09:46:40 +0800

    layout changes

--- src/App.js
+++ src/App.js
@@ -171,6 +171,15 @@ function App() {
     }
 
     const keys = data[0];
+
+    const hasPostTime = keys.includes("Post time");
+    const hasReach = keys.includes("Reach");
+
+    if (!hasPostTime || !hasReach) {
+      alert('Invalid CSV. \n\nData should have "Post time" and "Reach" as columns.');
+      return;
+    }
+
     const values = data.slice(1);
     const dataObj = values
       .map((value) => {
@@ -224,114 +233,120 @@ function App() {
   };
 
   return (
-    <main className="h-full w-full flex justify-center container p-8 overflow-x-hidden">
-      <div>
-        <form className="flex flex-col" onSubmit={onSubmit}>
-          <label htmlFor="csv" className="font-semibold mb-2 text-gray-700">
-            Upload CSV:
-          </label>
-          <input id="csv" name="csv" type="file" accept="text/csv" className="pb-4" required />
+    <main className="h-full w-full flex justify-center p-8 overflow-x-hidden">
+      <div className="w-full container max-w-3xl">
+        <div className="w-full">
+          <form className="w-full flex flex-col" onSubmit={onSubmit}>
+            <label htmlFor="csv" className="font-semibold mb-2 text-gray-700">
+              Upload CSV:
+            </label>
+            <input id="csv" name="csv" type="file" accept="text/csv" className="pb-4" required />
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 mt-2 gap-8">
-            <div>
-              <label htmlFor="minColor" className="font-semibold mb-2 text-gray-700">
-                Min value color:
-              </label>
-              <div className="flex items-center">
-                <input
-                  type="color"
-                  id="minColor"
-                  name="minColor"
-                  defaultValue={colors.minColor}
-                  required
-                  onChange={onChangeColor}
-                />
-                <span className="ml-2 uppercase text-sm">{colors.minColor}</span>
+            <div className="grid grid-cols-1 sm:grid-cols-2 mt-2 gap-x-8 gap-y-4">
+              <div>
+                <label htmlFor="minColor" className="font-semibold mb-2 text-gray-700">
+                  Min value color:
+                </label>
+                <div className="flex items-center">
+                  <input
+                    type="color"
+                    id="minColor"
+                    name="minColor"
+                    defaultValue={colors.minColor}
+                    required
+                    onChange={onChangeColor}
+                  />
+                  <span className="ml-2 uppercase text-sm">{colors.minColor}</span>
+                </div>
               </div>
 
-              <label htmlFor="width" className="block font-semibold mb-2 text-gray-700 mt-2">
-                Table width:
-              </label>
               <div>
-                <input
-                  type="number"
-                  name="width"
-                  id="width"
-                  className="block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
-                  defaultValue={dimensions.width}
-                  min="100"
-                  max="4000"
-                  step="1"
-                  onChange={onChangeDimension}
-                  required
-                />
+                <label htmlFor="maxColor" className="font-semibold mb-2 text-gray-700">
+                  Max value color:
+                </label>
+                <div className="flex items-center">
+                  <input
+                    type="color"
+                    id="maxColor"
+                    name="maxColor"
+                    defaultValue={colors.maxColor}
+                    required
+                    onChange={onChangeColor}
+                  />
+                  <span className="ml-2 uppercase text-sm">{colors.maxColor}</span>
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="width" className="block font-semibold mb-2 text-gray-700 mt-2">
+                  Table width:
+                </label>
+                <div>
+                  <input
+                    type="number"
+                    name="width"
+                    id="width"
+                    className="block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
+                    defaultValue={dimensions.width}
+                    min="100"
+                    max="4000"
+                    step="1"
+                    onChange={onChangeDimension}
+                    required
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="height" className="block font-semibold mb-2 text-gray-700 mt-2">
+                  Table height:
+                </label>
+                <div>
+                  <input
+                    type="number"
+                    name="height"
+                    id="height"
+                    className="block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
+                    defaultValue={dimensions.height}
+                    min="100"
+                    max="4000"
+                    step="1"
+                    onChange={onChangeDimension}
+                    required
+                  />
+                </div>
               </div>
             </div>
 
-            <div>
-              <label htmlFor="maxColor" className="font-semibold mb-2 text-gray-700">
-                Max value color:
-              </label>
-              <div className="flex items-center">
-                <input
-                  type="color"
-                  id="maxColor"
-                  name="maxColor"
-                  defaultValue={colors.maxColor}
-                  required
-                  onChange={onChangeColor}
-                />
-                <span className="ml-2 uppercase text-sm">{colors.maxColor}</span>
-              </div>
-
-              <label htmlFor="height" className="block font-semibold mb-2 text-gray-700 mt-2">
-                Table height:
-              </label>
-              <div>
-                <input
-                  type="number"
-                  name="height"
-                  id="height"
-                  className="block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
-                  defaultValue={dimensions.height}
-                  min="100"
-                  max="4000"
-                  step="1"
-                  onChange={onChangeDimension}
-                  required
-                />
-              </div>
+            <div className="mt-8 flex justify-center items-center">
+              <button
+                type="submit"
+                className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none active:bg-indigo-500"
+              >
+                Generate Table
+              </button>
             </div>
-          </div>
+          </form>
 
-          <div className="mt-8 flex justify-center items-center">
-            <button
-              type="submit"
-              className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none active:bg-indigo-500"
-            >
-              Generate Table
-            </button>
-          </div>
-        </form>
+          {imgSrc && (
+            <div className="max-w-3xl object-contain my-4 pb-8 flex flex-col items-center">
+              <img
+                src={imgSrc}
+                alt="Generated table"
+                width={dimensions.width}
+                height={dimensions.height}
+              />
 
-        {imgSrc && (
-          <div className="max-w-3xl object-contain my-4 pb-8 flex flex-col items-center">
-            <img
-              src={imgSrc}
-              alt="Generated table"
-              width={dimensions.width}
-              height={dimensions.height}
-            />
-
-            <button
-              type="button"
-              className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none active:bg-indigo-500 mt-4"
-              onClick={onClickDownload}
-            >
-              Download Image
-            </button>
-          </div>
-        )}
+              <button
+                type="button"
+                className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none active:bg-indigo-500 mt-4"
+                onClick={onClickDownload}
+              >
+                Download Image
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </main>
   );
